ef6fa8bc7e96e57e75996a38efc862de105a5455
Roma Bondalovskiy <bondalovskiyroma@gmail.com>
2024-10-03 14:40:24 +0300

    lab3 1.2 sticky elem color fix

--- lab3/input.js
+++ lab3/input.js
@@ -3,7 +3,7 @@ window.onload = function() {
     let offsetX, offsetY;
     let isSticky = false;
     let initialPosition = new Map();
-
+    let initialColor = new Map();
 
     function startDrag(event) {
         if (!isSticky) {
@@ -14,40 +14,37 @@ window.onload = function() {
     }
 
     function drag(event) {
-        if (selectedElement && !isSticky) {
-            selectedElement.style.left = event.clientX - offsetX + 'px';
-            selectedElement.style.top = event.clientY - offsetY + 'px';
-        }
-        if (isSticky && selectedElement) {
+        if (selectedElement) {
             selectedElement.style.left = event.clientX - offsetX + 'px';
             selectedElement.style.top = event.clientY - offsetY + 'px';
         }
     }
 
     function stopDrag() {
-        selectedElement = null;
+        if (isSticky) {
+            selectedElement.style.backgroundColor = initialColor.get(selectedElement);
+            selectedElement = null;
+        }
     }
 
-    //double click
     function makeSticky(event) {
         if (!isSticky) {
             selectedElement = event.target;
             isSticky = true;
             offsetX = event.clientX - selectedElement.getBoundingClientRect().left;
             offsetY = event.clientY - selectedElement.getBoundingClientRect().top;
-            selectedElement.style.backgroundColor = 'blue';
+            selectedElement.style.backgroundColor = 'blue'; // изменение цвета
         } else {
             isSticky = false;
-            selectedElement.style.backgroundColor = 'red';
+            selectedElement.style.backgroundColor = initialColor.get(selectedElement);
             selectedElement = null;
         }
     }
 
-    //esc
     function handleKeyUp(event) {
         if (event.key === 'Escape' && selectedElement) {
             isSticky = false;
-            selectedElement.style.backgroundColor = 'red';
+            selectedElement.style.backgroundColor = initialColor.get(selectedElement);
             let { top, left } = initialPosition.get(selectedElement);
             selectedElement.style.left = left;
             selectedElement.style.top = top;
@@ -55,17 +52,19 @@ window.onload = function() {
         }
     }
 
+    //на все таргеты
     document.querySelectorAll('.target').forEach((target) => {
         initialPosition.set(target, {
             top: target.style.top,
             left: target.style.left
         });
 
+        initialColor.set(target, target.style.backgroundColor);
+
         target.addEventListener('mousedown', startDrag);
         target.addEventListener('dblclick', makeSticky);
     });
 
-    //event
     document.addEventListener('mousemove', drag);
     document.addEventListener('mouseup', stopDrag);
     document.addEventListener('keyup', handleKeyUp);
